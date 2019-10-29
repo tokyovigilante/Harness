033269b14238c0006d5d12c26fcee236c2d7acae
Ryan Walklin <ryan@testtoast.com>
2019-10-29 13:44:55 +0100

    Add ** exponentiation operator

---
+++ Sources/Harness/Utilities/Math.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+precedencegroup ExponentiationPrecedence {
+    higherThan: MultiplicationPrecedence
+    associativity: left
+}
+
+infix operator **: ExponentiationPrecedence
+
+func ** (_ base: Double, _ exp: Double) -> Double {
+  return pow(base, exp)
+}
+
+func ** (_ base: Float, _ exp: Float) -> Float {
+  return pow(base, exp)
+}
+
+func ** (_ base: Int, _ exp: Int) -> Int {
+  return Int(pow(Double(base), Double(exp)))
+}
