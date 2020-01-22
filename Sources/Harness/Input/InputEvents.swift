f7f2aec594967767b8890e63f0299c2b1c29a006
Ryan Walklin <ryan@testtoast.com>
2020-01-22 22:34:01 +1100

    Restructure InputEvents

--- Sources/Harness/Input/InputEvents.swift
+++ Sources/Harness/Input/InputEvents.swift
@@ -5,6 +5,25 @@ public enum PointerEventType {
     case button
 }
 
+public struct InputModifier: OptionSet {
+    public var rawValue: UInt32
+
+    public init(rawValue: UInt32) {
+        self.rawValue = rawValue
+    }
+
+    public static let control = InputModifier(rawValue: 1 << 0)
+    public static let shift   = InputModifier(rawValue: 1 << 1)
+    public static let alt     = InputModifier(rawValue: 1 << 2)
+    public static let meta    = InputModifier(rawValue: 1 << 3)
+
+    public static let button1  = InputModifier(rawValue: 1 << 20)
+    public static let button2  = InputModifier(rawValue: 1 << 21)
+    public static let button3  = InputModifier(rawValue: 1 << 22)
+    public static let button4  = InputModifier(rawValue: 1 << 23)
+    public static let button5  = InputModifier(rawValue: 1 << 24)
+}
+
 public struct InputPointerEvent {
     public var type: PointerEventType
     public var time: Int
@@ -35,29 +54,11 @@ public struct InputAxisEvent {
     public var modifiers: InputModifier
 }
 
-public struct InputModifier: OptionSet {
-    public var rawValue: UInt32
-
-    public init(rawValue: UInt32) {
-        self.rawValue = rawValue
-    }
-
-    public static let control = InputModifier(rawValue: 1 << 0)
-    public static let shift   = InputModifier(rawValue: 1 << 1)
-    public static let alt     = InputModifier(rawValue: 1 << 2)
-    public static let meta    = InputModifier(rawValue: 1 << 3)
-
-    public static let button1  = InputModifier(rawValue: 1 << 20)
-    public static let button2  = InputModifier(rawValue: 1 << 21)
-    public static let button3  = InputModifier(rawValue: 1 << 22)
-    public static let button4  = InputModifier(rawValue: 1 << 23)
-    public static let button5  = InputModifier(rawValue: 1 << 24)
-}
-
 public struct InputKeyboardEvent {
     public var time: UInt32
     public var keyCode: UInt32
     public var hardwareKeyCode: UInt32
     public var pressed: Bool
     public var modifiers: InputModifier
-};
+}
+
