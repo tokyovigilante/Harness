c723d118ccad1a4114f34a47366710503619b230
Ryan Walklin <ryan@testtoast.com>
2020-01-27 19:51:19 +1100

    Add MimeTypes

---
+++ Sources/Harness/Types/MimeTypes.swift
@@ -0,0 +1,9 @@
+
+public let MimeTypes: [String: String] = [
+    "html": "text/html",
+    "css": "text/css",
+    "otf": "application/x-font-opentype",
+    "json": "application/json",
+    "js": "text/javascript",
+    "svg": "image/svg"
+]
